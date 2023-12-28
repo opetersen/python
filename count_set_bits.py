f46a7f8f3ec8500ae072e6cb94ee9448625f0c2b
opetersen <opetersen@runbox.com>
2023-12-28 01:33:45 +0100

    speed up, clean up

--- count_set_bits.py
+++ count_set_bits.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
+import timeit
 
-def bit_counter(num):
 
-    count = 0
-
-    for i in range(num):
-        count += f'{i:b}'.count('1')
-
-    print(count)
+def bit_counter_bit_count(num):
+    return sum(i.bit_count() for i in range(num))
 
 
 if __name__ == '__main__':
-    bit_counter(1000000)
+    num = 10000000
+    start = timeit.default_timer()
+    data = bit_counter_bit_count(num)
+    end = timeit.default_timer()
+    print(f"Time taken by bin method: {end - start} seconds")
+    print(f"Number of set bits: {data}")
